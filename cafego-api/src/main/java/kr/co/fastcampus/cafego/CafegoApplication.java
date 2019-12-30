d4c8dabbabdb5bd622242d53995dae5ebb2682b5
WhiteOwl314 <parksj914@naver.com>
2019-12-30 13:27:20 +0900

    Initial commit

---
+++ cafego-api/src/main/java/kr/co/fastcampus/cafego/CafegoApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.cafego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafegoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafegoApplication.class, args);
+	}
+
+}
