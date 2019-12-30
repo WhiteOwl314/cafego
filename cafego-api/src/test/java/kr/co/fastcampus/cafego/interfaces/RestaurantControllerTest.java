98078b3702c3f27c9a93eb166467da61aed08262
WhiteOwl314 <parksj914@naver.com>
2019-12-30 13:58:57 +0900

    Desgin RestaurantControllerTest

--- cafego-api/src/test/java/kr/co/fastcampus/cafego/interfaces/RestaurantControllerTest.java
+++ cafego-api/src/test/java/kr/co/fastcampus/cafego/interfaces/RestaurantControllerTest.java
@@ -1,7 +1,27 @@
 package kr.co.fastcampus.cafego.interfaces;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RestaurantController.class)
 class RestaurantControllerTest {
 
+   @Autowired
+   private MockMvc mvc;
+
+   @Test
+    public void list() throws Exception {
+
+       mvc.perform(get("/restaurants"))
+               .andExpect(status().isOk());
+   }
 }
