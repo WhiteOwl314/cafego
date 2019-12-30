95ee24d55605d5495e71a598f640b64660add425
WhiteOwl314 <parksj914@naver.com>
2019-12-30 13:33:03 +0900

    create Restaurant class and RestaurantTest

---
+++ cafego-api/src/main/java/kr/co/fastcampus/cafego/domain/Restaurant.java
@@ -0,0 +1,5 @@
+package kr.co.fastcampus.cafego.domain;
+
+public class Restaurant {
+
+}
---
+++ cafego-api/src/test/java/kr/co/fastcampus/cafego/domain/RestaurantTest.java
@@ -0,0 +1,7 @@
+package kr.co.fastcampus.cafego.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+
+}
