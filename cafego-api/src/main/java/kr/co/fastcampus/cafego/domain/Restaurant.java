19f453b1e89ad27df43b54cfde4cbded0811c309
WhiteOwl314 <parksj914@naver.com>
2019-12-30 13:47:03 +0900

    Test creation class

--- cafego-api/src/main/java/kr/co/fastcampus/cafego/domain/Restaurant.java
+++ cafego-api/src/main/java/kr/co/fastcampus/cafego/domain/Restaurant.java
@@ -2,4 +2,19 @@ package kr.co.fastcampus.cafego.domain;
 
 public class Restaurant {
 
+    private String name;
+    private String address;
+
+    public Restaurant(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
--- cafego-api/src/test/java/kr/co/fastcampus/cafego/domain/RestaurantTest.java
+++ cafego-api/src/test/java/kr/co/fastcampus/cafego/domain/RestaurantTest.java
@@ -1,7 +1,19 @@
 package kr.co.fastcampus.cafego.domain;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RestaurantTest {
+
+    @Test
+    public void creation(){
+        Restaurant restaurant = new Restaurant("1리터","대전");
+        assertThat(restaurant.getName(), is("1리터"));
+        assertThat(restaurant.getAddress(), is("대전"));
+
+    }
 
 }
